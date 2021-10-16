c8b77ec55b0f514777bfea6d92cc99a9e507b22b
xarantolus <xarantolus@protonmail.com>
2021-10-16 10:28:30 +0200

    Launch list: load more when scrolling down

--- lib/pages/upcoming_launches.dart
+++ lib/pages/upcoming_launches.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:rockit/launch_library/api.dart';
 import 'package:rockit/launch_library/json_convert.dart';
 import 'package:rockit/widgets/launch.dart';
+import 'package:loadmore/loadmore.dart';
 
 class UpcomingLaunchesPage extends StatefulWidget {
   UpcomingLaunchesPage({Key? key}) : super(key: key);
@@ -26,12 +27,10 @@ class _UpcomingLaunchesPageState extends State<UpcomingLaunchesPage> {
                 child: Text("There are no items"),
               );
             } else {
-              return ListView.builder(
-                itemCount: results!.length,
-                cacheExtent: MediaQuery.of(context).size.height * 5,
-                itemBuilder: (context, index) {
-                  return LaunchWidget(results[index]);
-                },
+              return LaunchesList(
+                results!,
+                snapshot.data!.next,
+                widget.service,
               );
             }
           } else if (snapshot.hasError) {
@@ -40,6 +39,85 @@ class _UpcomingLaunchesPageState extends State<UpcomingLaunchesPage> {
             return const CircularProgressIndicator();
           }
         },
+      ),
+    );
+  }
+}
+
+class LaunchesList extends StatefulWidget {
+  const LaunchesList(this.initialLaunches, this.initialNextURL, this.service,
+      {Key? key})
+      : super(key: key);
+
+  final LaunchLibraryAPI service;
+
+  final List<Launch> initialLaunches;
+  final String? initialNextURL;
+
+  @override
+  _LaunchesListState createState() => _LaunchesListState();
+}
+
+class _LaunchesListState extends State<LaunchesList> {
+  late List<Launch> launches = widget.initialLaunches;
+  late String? nextURL = widget.initialNextURL;
+
+  Future<void> _updateLaunches([bool? refresh]) async {
+    var _launches = launches;
+
+    var _nextURL = nextURL;
+    if (refresh == true) {
+      _nextURL = null;
+      _launches.clear();
+    }
+
+    var _newLaunches = await widget.service.upcomingLaunches(_nextURL);
+
+    _launches.addAll(_newLaunches.results ?? []);
+
+    setState(() {
+      launches = _launches;
+      nextURL = _newLaunches.next;
+    });
+  }
+
+  bool loadingMore = false;
+
+  Future<bool> _loadMore() async {
+    try {
+      await _updateLaunches(false);
+    } catch (_) {
+      return false;
+    }
+    return true;
+  }
+
+  String _buildLoadingText(LoadMoreStatus status) =>
+      const {
+        LoadMoreStatus.fail: "Failed to load. Tap to try again.",
+        LoadMoreStatus.idle: "Waiting...",
+        LoadMoreStatus.loading: "Loading more launches...",
+        LoadMoreStatus.nomore: "You've reached the end of the list.",
+      }[status] ??
+      "Unknown";
+
+  @override
+  Widget build(BuildContext context) {
+    return RefreshIndicator(
+      onRefresh: () async {
+        await _updateLaunches(true);
+      },
+      child: LoadMore(
+        isFinish: nextURL == null,
+        onLoadMore: _loadMore,
+        child: ListView.builder(
+          itemCount: launches.length,
+          cacheExtent: MediaQuery.of(context).size.height * 5,
+          itemBuilder: (context, index) {
+            return LaunchWidget(launches[index]);
+          },
+        ),
+        textBuilder: _buildLoadingText,
       ),
     );
   }
