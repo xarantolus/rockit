935bc8a4e68bd43390174a325fe71a46e17fe5ad
xarantolus <xarantolus@protonmail.com>
2021-10-31 10:12:43 +0100

    Fix handler

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:rockit/background/handler.dart';
 import 'package:rockit/pages/article_listing.dart';
 import 'package:rockit/pages/credits_page.dart';
 import 'package:rockit/pages/upcoming_events_listing.dart';
