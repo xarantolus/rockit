4740b73961e12228214b4ab8ac0f71f881f71271
xarantolus <xarantolus@protonmail.com>
2021-11-13 20:03:16 +0100

    Minimal simplifications

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ void main() async {
   // Allow a bit more render image cache, this makes images reload less
   // It's a bit annoying to reduce the problem like this instead of being able to solve it in a good way.
   // See https://github.com/flutter/flutter/issues/68700 for more details
-  PaintingBinding.instance!.imageCache!.maximumSizeBytes = 300 << 20;
+  PaintingBinding.instance!.imageCache!.maximumSizeBytes = 500 << 20; // 500 MiB
 
   runApp(const RockItApp());
 }
